275468ea6e2b3956c114e6c78edca009f04fa886
Michael Kolber <michaelkolber@users.noreply.github.com>
2019-08-15 00:01:58 -0400

    Set up telegraf

---
+++ src/ts/app.ts
@@ -0,0 +1,46 @@
+require('dotenv').config();
+
+const express = require('express');
+const fs = require('fs');
+const Telegraf = require('telegraf');
+
+// Make sure a token is defined
+if (process.env.TOKEN == undefined) {
+    throw "You must specify TOKEN in a .env file in the root directory.";
+}
+
+
+const bot = new Telegraf(process.env.TOKEN);
+
+// TLS options
+const tlsOptions = {
+    key: fs.readFileSync(process.env.TLS_KEY_PATH),
+    cert: fs.readFileSync(process.env.TLS_CERT_PATH),
+    // ca: [
+    //   // This is necessary only if the client uses the self-signed certificate.
+    //   fs.readFileSync('client-cert.pem')
+    // ]
+}
+  
+// Set telegram webhook
+bot.telegram.setWebhook(process.env.LISTEN_URL, {
+  source: process.env.TLS_CERT_PATH
+})
+
+
+
+bot.start((ctx) => ctx.reply('Welcome'))
+bot.help((ctx) => ctx.reply('Send me a sticker'))
+bot.on('sticker', (ctx) => ctx.reply('👍'))
+bot.hears('hi', (ctx) => ctx.reply('Hey there'))
+bot.launch()
+
+
+
+
+// const app = express()
+// const port = 3000
+
+// app.get('/', (req, res) => res.send(`Running`));
+
+// app.listen(port, () => console.log(`App listening on port ${port}!`));

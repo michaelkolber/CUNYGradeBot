ef8eb77a118d47d82a15fc9e985ebca6ec7f04de
Michael Kolber <michaelkolber@users.noreply.github.com>
2019-08-16 04:50:23 +0000

    Log when parse functions are called

--- app/parsers.js
+++ app/parsers.js
@@ -2,11 +2,13 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 // Middleware to parse a class message. Start with the least ambiguous token and move on from there
 function parseClass(ctx, next) {
+    console.log('parseClass called');
     next(ctx);
 }
 exports.parseClass = parseClass;
 // Middleware to parse a professor message
 function parseProfessor(ctx, next) {
+    console.log('parseProfessor called');
     next(ctx);
 }
 exports.parseProfessor = parseProfessor;
--- src/ts/parsers.ts
+++ src/ts/parsers.ts
@@ -1,13 +1,13 @@
 // Middleware to parse a class message. Start with the least ambiguous token and move on from there
 function parseClass(ctx, next) {
-    
+    console.log('parseClass called');
     next(ctx);
 }
 
 
 // Middleware to parse a professor message
 function parseProfessor(ctx, next) {
-    
+    console.log('parseProfessor called');
     next(ctx);
 }
 

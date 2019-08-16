4f2edc2aa6ec6ba0c6dfde18cbb34ddd4d6aa812
Michael Kolber <michaelkolber@users.noreply.github.com>
2019-08-16 02:14:49 -0400

    Add regular expressions

--- app/parsers.js
+++ app/parsers.js
@@ -1,5 +1,12 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
+// Regular expressions
+let classRegexes = {
+    'course': /[a-z]+ ?\d{2,4}/,
+    'professor': /[a-z]+,?( [a-z]+)?/,
+    'section': /sec(tion)? ?\d/,
+    'semester': /(f|fall|s|spring) ?'?(\d\d){1,2}/
+};
 // Middleware to parse a class message. Start with the least ambiguous token and move on from there
 function parseClass(ctx, next) {
     console.log('parseClass called');

0931e85000c4d476ece45b63d2cad01c31275d47
Michael Kolber <michaelkolber@users.noreply.github.com>
2019-08-22 05:22:41 +0000

    Add pino logging, reformat a bit

--- app/bot.js
+++ app/bot.js
@@ -9,10 +9,14 @@ var __importStar = (this && this.__importStar) || function (mod) {
 Object.defineProperty(exports, "__esModule", { value: true });
 // Load environment variables
 require('dotenv').config();
+// Requires
+const fs = require('fs');
+const pino = require('pino');
+const Telegraf = require('telegraf');
 // Imports
 const parsers = __importStar(require("./parsers"));
-const fs = require('fs');
-const Telegraf = require('telegraf');
+// Set up logger
+const logger = pino({ level: process.env.LOG_LEVEL || 'info' });
 // Make sure a bot token is defined
 if (process.env.TOKEN == undefined) {
     throw new Error('You must specify TOKEN in a .env file in the root directory.');
@@ -20,8 +24,8 @@ if (process.env.TOKEN == undefined) {
 // Initialize the bot
 const bot = new Telegraf(process.env.TOKEN);
 /* Set the webhook on Telegram's side. Since we're using a valid cert, we don't need to
- * include our public certificate.
- */
+* include our public certificate.
+*/
 bot.telegram.setWebhook('https://' +
     process.env.LISTEN_HOST +
     ':' +
@@ -31,21 +35,21 @@ bot.telegram.setWebhook('https://' +
 bot.telegram.webhookReply = false;
 // Print all messages to the console
 bot.use((ctx, next) => {
-    console.log(ctx.message);
+    logger.debug(ctx.message);
     next(ctx);
 });
 // Global commands
 bot.start((ctx) => ctx.reply('Welcome'));
-bot.help((ctx) => ctx.reply('Send me a sticker'));
+bot.help((ctx) => ctx.reply('Use /class or /professor'));
 // Bot commands
 bot.command('class', parsers.parseClassMessage);
 bot.command('professor', parsers.parseProfessorMessage);
 // TLS options
 const tlsOptions = {
-    key: fs.readFileSync(process.env.TLS_KEY_PATH),
     cert: fs.readFileSync(process.env.TLS_CERT_PATH),
+    key: fs.readFileSync(process.env.TLS_KEY_PATH),
 };
 // Start listening
 bot.startWebhook(process.env.LISTEN_PATH, tlsOptions, process.env.LISTEN_PORT); // TODO: Use secret path?
-console.log('Bot is listening!');
+logger.info(`Gradebot is listening! Host: ${process.env.LISTEN_HOST} | Port: ${process.env.LISTEN_PORT}`);
 //# sourceMappingURL=bot.js.map
--- src/ts/bot.ts
+++ src/ts/bot.ts
@@ -1,26 +1,30 @@
 // Load environment variables
 require('dotenv').config();
 
+// Requires
+const fs = require('fs');
+const pino = require('pino');
+const Telegraf = require('telegraf');
+
 // Imports
 import * as parsers from './parsers'
 
-const fs = require('fs');
-const Telegraf = require('telegraf');
 
+
+// Set up logger
+const logger = pino({level: process.env.LOG_LEVEL || 'info'});
 
 // Make sure a bot token is defined
 if (process.env.TOKEN == undefined) {
     throw new Error('You must specify TOKEN in a .env file in the root directory.');
 }
 
-
 // Initialize the bot
 const bot = new Telegraf(process.env.TOKEN);
 
-
 /* Set the webhook on Telegram's side. Since we're using a valid cert, we don't need to 
- * include our public certificate.
- */
+* include our public certificate.
+*/
 bot.telegram.setWebhook('https://' + 
                         process.env.LISTEN_HOST + 
                         ':' + 
@@ -30,17 +34,16 @@ bot.telegram.setWebhook('https://' +
 // Disable webhook replies, as they don't seem to work.
 bot.telegram.webhookReply = false;
 
-
 // Print all messages to the console
 bot.use((ctx, next) => {
-    console.log(ctx.message);
+    logger.debug(ctx.message);
     next(ctx);
 });
 
 
 // Global commands
 bot.start((ctx) => ctx.reply('Welcome'))
-bot.help((ctx) => ctx.reply('Send me a sticker'))
+bot.help((ctx) => ctx.reply('Use /class or /professor'))
 
 // Bot commands
 bot.command('class', parsers.parseClassMessage);
@@ -49,11 +52,11 @@ bot.command('professor', parsers.parseProfessorMessage);
 
 // TLS options
 const tlsOptions = {
-    key: fs.readFileSync(process.env.TLS_KEY_PATH),
     cert: fs.readFileSync(process.env.TLS_CERT_PATH),
-}
+    key: fs.readFileSync(process.env.TLS_KEY_PATH),
+};
 
 // Start listening
 bot.startWebhook(process.env.LISTEN_PATH, tlsOptions, process.env.LISTEN_PORT);  // TODO: Use secret path?
 
-console.log('Bot is listening!');
+logger.info(`Gradebot is listening! Host: ${process.env.LISTEN_HOST} | Port: ${process.env.LISTEN_PORT}`);

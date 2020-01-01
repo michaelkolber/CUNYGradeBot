739a3b1856d9196cb0dcb0d8ce5aa880eab54974
Michael Kolber <michaelkolber@users.noreply.github.com>
2019-12-31 21:30:24 -0500

    Clean up namespaces and unnecessary variables

--- app/db/server.js
+++ app/db/server.js
@@ -8,23 +8,20 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 const express = require("express");
 const dbConnection = require("./dbConnection"); // The connection lives here and is `require`ed whenever needed. Relies on Node's module caching.
-const helpers_1 = require("./helpers");
+const helpers = require("./helpers");
 const app = express();
 const port = process.env.PORT || 1900;
 // Make sure JSON is parsed properly
 app.use(express.json());
 // Set up routes
 const apiRouter = express.Router(); // Tie everything to this router so that we can run everything through '/api'
+apiRouter.use('/classes', require('./routes/classes'));
+apiRouter.use('/professors', require('./routes/professors'));
+apiRouter.use('/search', require('./routes/search'));
 app.use('/api', apiRouter);
-const classesRouter = require("./routes/classes");
-apiRouter.use('/classes', classesRouter);
-const professorsRouter = require("./routes/professors");
-apiRouter.use('/professors', professorsRouter);
-const searchRouter = require("./routes/search");
-apiRouter.use('/search', searchRouter);
 // Catch-all. Also serves as an easy way to make sure the API is up.
 app.all('/', (req, res) => {
-    const message = helpers_1.createErrorMessage("All requests must go through '/api'.");
+    const message = helpers.createErrorMessage("All requests must go through '/api'.");
     res.status(403).json(message);
 });
 // Connect to the database before starting up the server
